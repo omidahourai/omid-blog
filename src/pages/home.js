a628972af1e4abf7581ed98cedb99091a1042e48
Omid Ahourai <oahourai@gmail.com>
2018-01-14 12:11:26 -0300

    fb meta

--- src/pages/home.js
+++ src/pages/home.js
@@ -17,13 +17,13 @@ class HomePage extends Component {
         return {
             title: `Omid Ahourai's Blog`,
             meta: [
-                { name: 'description', content: 'Omid Ahourai is an Entrepreneur, Web and Mobile Apps and Games Developer, and Digital Nomad.' },
+                { name: 'description', content: 'Read about my life! Omid Ahourai is an Entrepreneur, Web and Mobile Apps and Games Developer, and Digital Nomad.' },
                 { name: 'keywords', content: 'omid ahourai, omid, ahourai, digital nomad, ardentkid, storyfork' },
-                // { property: 'og:site_name', content: `Blog - Omid Ahourai` },
-                // { property: 'og:type', content: 'article' },
-                // { property: 'og:title', content: title },
-                // { property: 'og:description', content: summary },
-                // { property: 'og:url', content: `http://omid.com/articles/${slug}` },
+                { property: 'og:site_name', content: `Blog - Omid Ahourai` },
+                { property: 'og:type', content: 'website' },
+                { property: 'og:title', content: `Omid Ahourai's Blog` },
+                { property: 'og:description', content: 'Read about my life! Omid Ahourai is an Entrepreneur, Web and Mobile Apps and Games Developer, and Digital Nomad.' },
+                { property: 'og:url', content: `http://omid.com/` },
                 // { property: 'og:image', content: `${hero.file.url}?w=1200&q=70` },
                 // { name: 'twitter:card', content: 'summary_large_image' },
                 // { name: 'twitter:title', content: title },
@@ -40,7 +40,7 @@ class HomePage extends Component {
     
     render() {
         const { data } = this.props
-        console.log('data',data)
+        // console.log('data',data)
         if ( !data ) {
             return <div>No Data :(</div>
         }
