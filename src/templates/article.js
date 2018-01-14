313977667f9bdd15085b6586f1246e10beb4e052
Omid Ahourai <oahourai@gmail.com>
2018-01-14 12:20:04 -0300

    twitter img

--- src/templates/article.js
+++ src/templates/article.js
@@ -47,7 +47,7 @@ class ArticleTemplate extends Component {
         { name: 'twitter:title', content: title },
         { name: 'twitter:description', content: summary },
         { name: 'twitter:url', content: `http://omid.com/articles/${slug}` },
-        { name: 'twitter:image', content: `${hero.file.url}?w=1200&q=70` },
+        { name: 'twitter:image', content: `http:${hero.file.url}?w=1200&q=70` },
         { name: 'twitter:label1', content: 'Written by' },
         { name: 'twitter:data1', content: `${ author.firstName } ${ author.lastName }` },
         { name: 'twitter:label2', content: 'Filed under' },
