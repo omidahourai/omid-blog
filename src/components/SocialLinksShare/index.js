1e1f2b1f1927f063cca057202657819c8805a02d
Omid Ahourai <oahourai@gmail.com>
2018-01-16 23:08:30 -0300

    cosmetics

--- src/components/SocialLinksShare/index.js
+++ src/components/SocialLinksShare/index.js
@@ -12,7 +12,7 @@ import { AnchorCircleIcon } from 'common/components'
 export const SocialLinksShare = ({ slug, title, category, imageUrl, tags }) => (
   <div className={`social-share ${ styles.share }`}>
     <AnchorCircleIcon 
-      className={styles.btn}
+      className={`${styles.btn} ${styles.facebook}`}
       href={encodeURI(`https://facebook.com/sharer/sharer.php?u=${ decodeURIComponent( `http://www.omid.com/articles/${ slug }`)}`)}
       rel="external"
       target="_blank"
@@ -20,7 +20,7 @@ export const SocialLinksShare = ({ slug, title, category, imageUrl, tags }) => (
       <FaFacebook />
     </AnchorCircleIcon>
     <AnchorCircleIcon 
-      className={styles.btn}
+      className={`${styles.btn} ${styles.twitter}`}
       href={encodeURI(`https://twitter.com/share?text=Check out this article by @omidahourai: ${ title }&hashtags=${ tags.join(',') }&url=${ `http://www.omid.com/articles/${ slug }` }`)}
       rel="external"
       target="_blank"
@@ -28,11 +28,11 @@ export const SocialLinksShare = ({ slug, title, category, imageUrl, tags }) => (
       <FaTwitter />
     </AnchorCircleIcon>
     <AnchorCircleIcon 
-      className={styles.btn}
+      className={`${styles.btn} ${styles.pinterest}`}
       href={`https://pinterest.com/pin/create/button/?url=http://omid.com/${lowerFirst(category)}/${slug}/&media=${imageUrl}`}
       rel="external"
       target="_blank"
-      title="Share on Twitter">
+      title="Share on Pinterest">
       <FaPinterestP />
     </AnchorCircleIcon>
   </div>
